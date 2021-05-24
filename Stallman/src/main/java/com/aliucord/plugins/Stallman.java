3677b44d9a9e837b72d1cf642d295f589db320a9
X1nto <burtkni@yandex.ru>
2021-05-24 19:02:52 +0400

    updated stallman

--- Stallman/src/main/java/com/aliucord/plugins/Stallman.java
+++ Stallman/src/main/java/com/aliucord/plugins/Stallman.java
@@ -6,6 +6,8 @@ import androidx.annotation.NonNull;
 
 import com.aliucord.entities.Plugin;
 import com.aliucord.api.CommandsAPI;
+import com.discord.api.commands.ApplicationCommandType;
+import com.discord.models.commands.ApplicationCommandOption;
 
 import java.util.*;
 
@@ -17,19 +19,20 @@ public class Stallman extends Plugin {
         Manifest manifest = new Manifest();
         manifest.authors = new Manifest.Author[] { new Manifest.Author("Xinto", 423915768191647755L) };
         manifest.description = "A plugin that replaces the word \"Linux\" in the Linux Copypasta with your desired string.";
-        manifest.version = "1.0.0";
+        manifest.version = "1.0.1";
         manifest.updateUrl = "https://raw.githubusercontent.com/X1nto/AliucordPlugins/builds/updater.json";
         return manifest;
     }
 
     @Override
     public void start(Context context) {
+        ApplicationCommandOption argument = new ApplicationCommandOption(ApplicationCommandType.STRING, "phrase", "A phrase to replace \"Linux\" with", null, true, true, null, null);
         commands.registerCommand(
             "stallman",
             "Replace \"Linux\" with your desired string",
-            Collections.singletonList(CommandsAPI.messageOption),
+            Collections.singletonList(argument),
             args -> {
-                Object desiredString = args.get("message");
+                Object desiredString = args.get("phrase");
                 String copypasta =
                         "I'd just like to interject for a moment. What you're referring to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX.\n\n" +
                         "Many computer users run a modified version of the GNU system every day, without realizing it. Through a peculiar turn of events, the version of GNU which is widely used today is often called \"Linux\", and many of its users are not aware that it is basically the GNU system, developed by the GNU Project.\n\n" +
