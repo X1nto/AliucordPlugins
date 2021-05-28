1f8f6d9425b0b4c3f057d62c55d7c9565049df6e
X1nto <burtkni@yandex.ru>
2021-05-28 16:24:23 +0400

    fixed ShowhiddenChannels bugs

--- ShowHiddenChannels/src/main/java/com/aliucord/plugins/ShowHiddenChannels.java
+++ ShowHiddenChannels/src/main/java/com/aliucord/plugins/ShowHiddenChannels.java
@@ -27,9 +27,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@SuppressWarnings("unused")
 public class ShowHiddenChannels extends Plugin {
 
-    private static final String suffix = "\u200B";
+    private static final String suffix = " ";
 
     private static final String channelListClass = "com.discord.widgets.channels.list.WidgetChannelListModel$Companion$guildListBuilder$$inlined$forEach$lambda$3";
     private static final String channelListMethod = "invoke";
@@ -56,7 +57,7 @@ public class ShowHiddenChannels extends Plugin {
         Manifest manifest = new Manifest();
         manifest.authors = new Manifest.Author[] { new Manifest.Author("Xinto",423915768191647755L) };
         manifest.description = "Allows you to see hidden channels in servers.";
-        manifest.version = "0.2.1";
+        manifest.version = "1.0.0";
         manifest.updateUrl = "https://raw.githubusercontent.com/X1nto/AliucordPlugins/builds/updater.json";
         return manifest;
     }
@@ -75,32 +76,32 @@ public class ShowHiddenChannels extends Plugin {
 
     private void patchChannelList() {
         patcher.patch(channelListClass, channelListMethod, (_this, args, ret) -> {
-            if (ret != null) return ret;
             WidgetChannelListModel$Companion$guildListBuilder$$inlined$forEach$lambda$3 lambda3 = (WidgetChannelListModel$Companion$guildListBuilder$$inlined$forEach$lambda$3) _this;
-            if (PermissionUtils.INSTANCE.hasAccess(lambda3.$channel, lambda3.$permissions)) return null;
-            WidgetChannelListModel.Companion.TextLikeChannelData textLikeChannelData = WidgetChannelListModel$Companion$guildListBuilder$$inlined$forEach$lambda$1.invoke$default(lambda3.$getTextLikeChannelData$1, lambda3.$channel, lambda3.$muted, null, 4, null);
-            if (textLikeChannelData == null || textLikeChannelData.getHide()) return null;
 
             Channel channel = lambda3.$channel;
             String channelName = channel.l();
 
-            boolean isChannelMuted = lambda3.$muted;
-
-            if (!PermissionUtils.INSTANCE.hasAccess(lambda3.$channel, lambda3.$permissions)) {
-                //Mute the channel to apply muted style
-                isChannelMuted = true;
-
-                if (!isChannelHidden(channelName)) {
-                    try {
-                        Field nameField = channel.getClass().getDeclaredField("name");
-                        nameField.setAccessible(true);
-                        nameField.set(channel, channelName + suffix);
-                    } catch (NoSuchFieldException | IllegalAccessException e) {
-                        e.printStackTrace();
-                    }
+            if (ret != null) {
+                //Rename back channel if it was previously hidden
+                //in order to let user see the channel after permission
+                //was granted.
+                if (!isChannelVisible(channelName)) {
+                    renameChannel(channel, removeSuffix(channelName, suffix));
                 }
+                return ret;
             }
-            return new ChannelListItemTextChannel(channel, textLikeChannelData.getSelected(), textLikeChannelData.getMentionCount(), textLikeChannelData.getUnread(), isChannelMuted, textLikeChannelData.getLocked(), lambda3.$channelsWithActiveThreads$inlined.contains(lambda3.$channel.g()));
+
+            if (PermissionUtils.INSTANCE.hasAccess(lambda3.$channel, lambda3.$permissions)) return null;
+
+            WidgetChannelListModel.Companion.TextLikeChannelData textLikeChannelData = WidgetChannelListModel$Companion$guildListBuilder$$inlined$forEach$lambda$1.invoke$default(lambda3.$getTextLikeChannelData$1, lambda3.$channel, lambda3.$muted, null, 4, null);
+
+            //Check if category is collapsed
+            if (textLikeChannelData == null || textLikeChannelData.getHide()) return null;
+
+            if (isChannelVisible(channelName)) {
+                renameChannel(channel, channelName + suffix);
+            }
+            return new ChannelListItemTextChannel(channel, textLikeChannelData.getSelected(), textLikeChannelData.getMentionCount(), textLikeChannelData.getUnread(), true, textLikeChannelData.getLocked(), lambda3.$channelsWithActiveThreads$inlined.contains(lambda3.$channel.g()));
         });
     }
 
@@ -110,19 +111,20 @@ public class ShowHiddenChannels extends Plugin {
             WidgetChannelsListAdapter.ItemChannelText _itemChannelText = (WidgetChannelsListAdapter.ItemChannelText) _this;
             Channel channel = textChannel.getChannel();
             String channelName = channel.l();
-            if (isChannelHidden(channelName)) {
-                try {
-                    Field bindingField = _itemChannelText.getClass().getDeclaredField("binding");
-                    bindingField.setAccessible(true);
-                    WidgetChannelsListItemChannelBinding binding = (WidgetChannelsListItemChannelBinding) bindingField.get(_itemChannelText);
-                    Drawable hiddenDrawable = ResourcesCompat.getDrawable(resources, resources.getIdentifier("ic_channel_hidden", "drawable", "com.aliucord.plugins"), null);
-                    assert binding != null;
-                    binding.b.setImageDrawable(hiddenDrawable);
-                    binding.d.setText(channelName);
-                    binding.a.setOnClickListener(null);
-                } catch (NoSuchFieldException | IllegalAccessException e) {
-                    e.printStackTrace();
-                }
+
+            if (isChannelVisible(channelName)) return null;
+
+            try {
+                Field bindingField = _itemChannelText.getClass().getDeclaredField("binding");
+                bindingField.setAccessible(true);
+                WidgetChannelsListItemChannelBinding binding = (WidgetChannelsListItemChannelBinding) bindingField.get(_itemChannelText);
+                Drawable hiddenDrawable = ResourcesCompat.getDrawable(resources, resources.getIdentifier("ic_channel_hidden", "drawable", "com.aliucord.plugins"), null);
+                assert binding != null;
+                binding.b.setImageDrawable(hiddenDrawable);
+                binding.d.setText(channelName);
+                binding.a.setOnClickListener(null);
+            } catch (NoSuchFieldException | IllegalAccessException e) {
+                e.printStackTrace();
             }
             return ret;
         });
@@ -131,32 +133,50 @@ public class ShowHiddenChannels extends Plugin {
     private void patchHiddenChannelActions() {
         patcher.patch(channelActionsClass, channelActionsMethod, (_this, args, ret) -> {
             WidgetChannelsListItemChannelActions.Model model = (WidgetChannelsListItemChannelActions.Model) args.get(0);
-
             Channel channel = model.getChannel();
-            if (isChannelHidden(channel.l())) {
-                try {
-                    Method getBinding = _this.getClass().getDeclaredMethod("getBinding");
-                    getBinding.setAccessible(true);
-                    WidgetChannelsListItemTextActionsBinding binding = (WidgetChannelsListItemTextActionsBinding) getBinding.invoke(_this);
 
-                    assert binding != null;
-                    binding.b.setVisibility(View.GONE);
-                    binding.c.setVisibility(View.GONE);
-                    binding.h.setVisibility(View.GONE);
-                    binding.j.setVisibility(View.GONE);
-                    binding.k.setVisibility(View.GONE);
-                    binding.g.setVisibility(View.GONE);
-                    binding.i.setVisibility(View.GONE);
-                    binding.e.setVisibility(View.GONE);
-                } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
-                    e.printStackTrace();
-                }
+            if (isChannelVisible(channel.l())) return null;
+
+            try {
+                Method getBinding = _this.getClass().getDeclaredMethod("getBinding");
+                getBinding.setAccessible(true);
+                WidgetChannelsListItemTextActionsBinding binding = (WidgetChannelsListItemTextActionsBinding) getBinding.invoke(_this);
+
+                assert binding != null;
+                binding.b.setVisibility(View.GONE);
+                binding.c.setVisibility(View.GONE);
+                binding.h.setVisibility(View.GONE);
+                binding.j.setVisibility(View.GONE);
+                binding.k.setVisibility(View.GONE);
+                binding.g.setVisibility(View.GONE);
+                binding.i.setVisibility(View.GONE);
+                binding.e.setVisibility(View.GONE);
+            } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+                e.printStackTrace();
             }
             return ret;
         });
     }
 
-    private boolean isChannelHidden(String channelName) {
-        return channelName.contains(suffix);
+    private void renameChannel(Channel channel, String newName) {
+        try {
+            Field nameField = channel.getClass().getDeclaredField("name");
+            nameField.setAccessible(true);
+            nameField.set(channel, newName);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
+
+    private String removeSuffix(String sourceString, String suffix) {
+        if (sourceString.endsWith(suffix)) {
+            return sourceString.substring(0, sourceString.length() - suffix.length());
+        }
+        return sourceString;
+    }
+
+    private boolean isChannelVisible(String channelName) {
+        return !channelName.contains(suffix);
+    }
+
 }
