4ca9c0a382f26fea756495541862e6dc87c663a1
X1nto <burtkni@yandex.ru>
2021-07-17 12:48:27 +0400

    cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 import com.android.build.gradle.BaseExtension
 
 buildscript {
@@ -8,8 +7,6 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:4.2.2")
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
     }
 }
 
