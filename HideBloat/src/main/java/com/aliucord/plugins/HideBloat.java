7485cfbcd39742f87654cde70b71d785c0e67cbd
X1nto <burtkni@yandex.ru>
2021-07-23 19:50:17 +0400

    bumped up version

--- HideBloat/src/main/java/com/aliucord/plugins/HideBloat.java
+++ HideBloat/src/main/java/com/aliucord/plugins/HideBloat.java
@@ -23,7 +23,7 @@ public class HideBloat extends Plugin {
         Manifest manifest = new Manifest();
         manifest.authors = new Manifest.Author[]{new Manifest.Author("Xinto", 423915768191647755L)};
         manifest.description = "Hides various discord bloat.";
-        manifest.version = "1.0.2";
+        manifest.version = "1.1.0";
         manifest.updateUrl = "https://raw.githubusercontent.com/X1nto/AliucordPlugins/builds/updater.json";
         return manifest;
     }
