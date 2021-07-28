394e2b1455a42bc65ad6f893d6a3abd007e00cf8
X1nto <burtkni@yandex.ru>
2021-07-28 12:31:40 +0400

    rephrase string

--- HideBloat/src/main/java/com/aliucord/plugins/hidebloat/util/Const.java
+++ HideBloat/src/main/java/com/aliucord/plugins/hidebloat/util/Const.java
@@ -11,7 +11,7 @@ public class Const {
     }
     
     public static class ViewName {
-        public static final String CALL_BUTTONS_NAME = "Call buttons";
+        public static final String CALL_BUTTONS_NAME = "Call buttons from user sheet";
         public static final String GIFT_BUTTON_NAME = "Nitro Gift button";
         public static final String INVITE_BUTTON_CHANNELS_NAME = "Invite button in channels list";
         public static final String INVITE_BUTTON_MEMBERS_NAME = "Invite button in members list";
