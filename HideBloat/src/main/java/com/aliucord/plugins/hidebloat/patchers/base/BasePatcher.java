8c8f87106448ef4536f7752b92aa34cb02ad1950
X1nto <burtkni@yanded.ru>
2021-08-04 19:34:36 +0400

    minor refactor

--- HideBloat/src/main/java/com/aliucord/plugins/hidebloat/patchers/base/BasePatcher.java
+++ HideBloat/src/main/java/com/aliucord/plugins/hidebloat/patchers/base/BasePatcher.java
@@ -3,6 +3,7 @@ package com.aliucord.plugins.hidebloat.patchers.base;
 import com.aliucord.api.PatcherAPI;
 import com.aliucord.api.SettingsAPI;
 import com.aliucord.patcher.PinePatchFn;
+import com.aliucord.plugins.hidebloat.util.Const;
 
 import java.lang.reflect.Member;
 
@@ -23,7 +24,7 @@ public abstract class BasePatcher {
 
     public void patch(PatcherAPI patcher, SettingsAPI sets) {
         patcher.patch(classMember, new PinePatchFn(callFrame -> {
-            if (sets.getBool(key, false)) {
+            if (sets.getBool(key, Const.PREFERENCE_DEFAULT_VALUE)) {
                 try {
                     patchBody(callFrame);
                 } catch (Exception e) {
--- HideBloat/src/main/java/com/aliucord/plugins/hidebloat/widgets/SwitchItem.java
+++ HideBloat/src/main/java/com/aliucord/plugins/hidebloat/widgets/SwitchItem.java
@@ -5,6 +5,7 @@ import android.content.Context;
 
 import com.aliucord.Utils;
 import com.aliucord.api.SettingsAPI;
+import com.aliucord.plugins.hidebloat.util.Const;
 import com.discord.utilities.color.ColorCompat;
 import com.discord.views.CheckedSetting;
 import com.google.android.material.card.MaterialCardView;
@@ -25,7 +26,7 @@ public class SwitchItem extends MaterialCardView {
         setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
 
         CheckedSetting setting = Utils.createCheckedSetting(context, CheckedSetting.ViewType.SWITCH, "Hide " + viewName, null);
-        setting.setChecked(settingsAPI.getBool(key, false));
+        setting.setChecked(settingsAPI.getBool(key, Const.PREFERENCE_DEFAULT_VALUE));
         setting.setOnCheckedListener(v -> settingsAPI.setBool(key, v));
 
         addView(setting);
