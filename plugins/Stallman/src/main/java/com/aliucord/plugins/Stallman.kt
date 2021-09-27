2078d472d7867d4315e80eb8294f1e9b1d5aa267
X1nto <burtkni@yandex.ru>
2021-09-27 15:06:00 +0400

    updated stallman

--- plugins/Stallman/src/main/java/com/aliucord/plugins/Stallman.kt
+++ plugins/Stallman/src/main/java/com/aliucord/plugins/Stallman.kt
@@ -1,21 +1,15 @@
 package com.aliucord.plugins
 
 import android.content.Context
+import com.aliucord.annotations.AliucordPlugin
 import com.aliucord.entities.Plugin
 import com.aliucord.entities.Plugin.Manifest.Author
 import com.aliucord.api.CommandsAPI.CommandResult
 import com.discord.api.commands.ApplicationCommandType
 import com.discord.models.commands.ApplicationCommandOption
 
+@AliucordPlugin
 class Stallman : Plugin() {
-
-    override fun getManifest() =
-        Manifest().apply {
-            authors = arrayOf(Author("Xinto", 423915768191647755L))
-            description = "Replace the word \"Linux\" in the Linux Copypasta with your desired phrase."
-            version = "1.0.2"
-            updateUrl = "https://raw.githubusercontent.com/X1nto/AliucordPlugins/builds/updater.json"
-        }
 
     override fun start(context: Context) {
         val argument = ApplicationCommandOption(
