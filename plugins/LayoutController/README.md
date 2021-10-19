732d39d4d0365dd12f222d93ba894cf22ca489fd
Diamond <33725716+DiamondMiner88@users.noreply.github.com>
2021-10-19 15:42:43 -0700

    feat: add WelcomeButtonPatch

--- plugins/LayoutController/README.md
+++ plugins/LayoutController/README.md
@@ -9,3 +9,4 @@ Lets you control the discord mobile layout.
 - Hide the notes box in the user sheet
 - Hide the call buttons in the user sheet
 - Remove the "Untrusted Domain" dialog
+- Removes the welcome sticker button on join messages
--- plugins/LayoutController/src/main/java/com/aliucord/plugins/layoutcontroller/util/Const.kt
+++ plugins/LayoutController/src/main/java/com/aliucord/plugins/layoutcontroller/util/Const.kt
@@ -10,6 +10,7 @@ object Key {
     const val SEARCH_BOX_KEY = "searchBoxDM"
     const val NOTES_KEY = "notes"
     const val UNTRUSTED_DOMAINS_KEY = "untrustedDomains"
+    const val WELCOME_BUTTON_KEY = "welcomeButton"
 }
 
 object Description {
@@ -20,4 +21,5 @@ object Description {
     const val SEARCH_BOX_DESCRIPTION = "Remove the search box in DM list"
     const val NOTES_DESCRIPTION = "Remove the notes box from the user sheet"
     const val UNTRUSTED_DOMAINS_DESCRIPTION = "Remove the untrusted domain dialog"
+    const val WELCOME_BUTTON_DESCRIPTION = "Remove the welcome button on join messages"
 }
