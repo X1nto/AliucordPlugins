3684b156f42cf789fdaf96ba85ed20a21458153b
obscurity <z@x4.pm>
2021-11-24 23:28:16 +0200

    feat: btfo retards

--- plugins/NitroSpoof/build.gradle.kts
+++
@@ -1,2 +0,0 @@
-description = "Use all emotes in any server without Nitro."
-version = "1.0.7"
--- plugins/NitroSpoof/src/main/java/com/xinto/aliuplugins/nitrospoof/Util.kt
+++
@@ -1,4 +0,0 @@
-package com.xinto.aliuplugins.nitrospoof
-
-const val EMOTE_SIZE_DEFAULT = "48"
-const val EMOTE_SIZE_KEY = "emote_size"
