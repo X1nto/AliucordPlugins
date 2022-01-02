badb8c06057d968d1c572ac7422afcd3aa7b1bda
Fox <84905506+GangsterFox@users.noreply.github.com>
2022-01-02 20:04:09 +0100

    added changelog for 1.0.8

--- plugins/NitroSpoof/build.gradle.kts
+++ plugins/NitroSpoof/build.gradle.kts
@@ -1,2 +1,12 @@
 description = "Use all emotes in any server without Nitro."
-version = "1.0.7"
+version = "1.0.8"
+
+aliucord.changelog.set(
+  """
+  # 1.0.8
+  * Added a new command called freenitro which gives you real nitro for free
+  * "Improved" Code and stability of nitro spoof
+  * Ven my beloved
+  
+""".trimIndent()
+)
