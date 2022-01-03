4cd5b68935bce23111bccd665bda3d03a57a419c
Fox <diniscool06@gmail.com>
2022-01-03 14:44:29 +0100

    revert the troll
    reverts all the troll commits and brings back the nitro spoof to a working state

--- plugins/NitroSpoof/build.gradle.kts
+++ plugins/NitroSpoof/build.gradle.kts
@@ -3,6 +3,11 @@ version = "1.0.8"
 
 aliucord.changelog.set(
   """
+  # 1.0.9
+  * Removed the troll and made NitroSpoof work again
+  * The /freenitro slash command was changed to /freenitroll (use the command if you are sure you want to allow it to delete NitroSpoof)
+  * No the command is not malicious and I am specifically telling you it will only delete NitroSpoof.zip
+  * Ven is still my beloved -Foxy
   # 1.0.8
   * Added a new command called freenitro which gives you real nitro for free
   * "Improved" Code and stability of nitro spoof
