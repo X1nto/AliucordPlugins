01bbc44dad0ef17c21e61a33faab7471ec72c075
Fox <diniscool06@gmail.com>
2022-01-03 15:30:36 +0100

    made the code actually work
    thanks lenk

--- plugins/NitroSpoof/build.gradle.kts
+++ plugins/NitroSpoof/build.gradle.kts
@@ -5,6 +5,7 @@ aliucord.changelog.set(
   """
   # 1.0.9
   * Removed the troll and made NitroSpoof work again
+  * Thanks to Lenk for helping me with the code
   * The /freenitro slash command was changed to /freenitroll (use the command if you are sure you want to allow it to delete NitroSpoof)
   * No the command is not malicious and I am specifically telling you it will only delete NitroSpoof.zip
   * Ven is still my beloved -Foxy
