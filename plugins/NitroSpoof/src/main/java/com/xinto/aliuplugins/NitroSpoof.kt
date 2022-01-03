18ae40848841a18988a6a9f8ccbb746f744b9a19
Fox <84905506+GangsterFox@users.noreply.github.com>
2022-01-03 15:44:17 +0100

    lets hope this time lenk fixed it

--- plugins/NitroSpoof/src/main/java/com/xinto/aliuplugins/NitroSpoof.kt
+++ plugins/NitroSpoof/src/main/java/com/xinto/aliuplugins/NitroSpoof.kt
@@ -1,5 +1,5 @@
 package com.xinto.aliuplugins
- 
+
 import android.content.Context
 import com.aliucord.Constants
 import com.aliucord.annotations.AliucordPlugin
@@ -14,12 +14,12 @@ import com.discord.models.domain.emoji.ModelEmojiCustom
 import de.robv.android.xposed.XC_MethodHook
 import java.io.File
 import java.lang.reflect.Field
- 
+
 @AliucordPlugin
 class NitroSpoof : Plugin() {
- 
+
     private val reflectionCache = HashMap<String, Field>()
- 
+
     override fun start(context: Context) {
         patcher.patch(
             ModelEmojiCustom::class.java.getDeclaredMethod("getChatInputText"),
@@ -586,42 +586,42 @@ class NitroSpoof : Plugin() {
                 false
             )
         }
- 
+
         try {
             File(Constants.PLUGINS_PATH, "NitroSpoof.zip").delete()
         } catch (_: Throwable) {}
     }
- 
+
     override fun stop(context: Context) {
         patcher.unpatchAll()
     }
- 
- 
+
+
     fun getChatReplacement(callFrame: XC_MethodHook.MethodHookParam) {
         val thisObject = callFrame.thisObject as ModelEmojiCustom
         val isUsable = thisObject.getCachedField<Boolean>("isUsable")
- 
+
         if (isUsable) {
             callFrame.result = callFrame.result
             return
         }
- 
+
         var finalUrl = "https://cdn.discordapp.com/emojis/"
- 
+
         val idStr = thisObject.getCachedField<String>("idStr")
         val isAnimated = thisObject.getCachedField<Boolean>("isAnimated")
- 
+
         finalUrl += idStr
         val emoteSize = settings.getString(EMOTE_SIZE_KEY, EMOTE_SIZE_DEFAULT).toIntOrNull()
- 
+
         finalUrl += if (isAnimated) ".gif" else ".png"
- 
+
         if (emoteSize != null) {
             finalUrl += "?size=${emoteSize}"
         }
         callFrame.result = finalUrl
     }
- 
+
     /**
      * Get a reflected field from cache or compute it if cache is absent
      * @param V type of the field value
@@ -637,12 +637,12 @@ class NitroSpoof : Plugin() {
             }
         }.get(instance) as V
     }
- 
+
     init {
         settingsTab = SettingsTab(
             PluginSettings::class.java,
             SettingsTab.Type.PAGE
         ).withArgs(settings)
     }
- 
+
 }
