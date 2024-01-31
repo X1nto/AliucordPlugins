c72742b64266a2a8e92ab2ddb93fa6c960348f72
OmegaSunkey <bronytecfan@gmail.com>
2024-01-30 19:30:56 -0500

    fix: my insanity

--- plugins/NitroSpoof/src/main/java/com/xinto/aliuplugins/nitrospoof/Util.kt
+++ plugins/NitroSpoof/src/main/java/com/xinto/aliuplugins/nitrospoof/Util.kt
@@ -6,4 +6,4 @@ const val EMOTE_SIZE_DEFAULT = "48"
 const val EMOTE_SIZE_KEY = "emote_size"
 const val EMPTY_CHAR = "[‎ ]"
 val permBlacklist = mapOf(ALIUCORD_GUILD_ID to "Aliucord")
-val servBlacklist = mutableMapOf(1015931589865246730 to "Vendetta
+val servBlacklist = mutableMapOf(1015931589865246730 to "Vendetta")
