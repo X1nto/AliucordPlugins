1fea4c42ce69e8d0cc3e7c104ac18b649c8f4fb8
Tornike Khintibidze <48173186+X1nto@users.noreply.github.com>
2024-10-13 12:33:10 +0400

    Update build.gradle.kts

--- plugins/NitroSpoof/build.gradle.kts
+++ plugins/NitroSpoof/build.gradle.kts
@@ -1,15 +1,16 @@
 description = "Use all emotes in any server without Nitro."
-version = "1.0.9"
+version = "1.0.10"
 
 aliucord.changelog.set("""
+  # 1.0.10
+  * Added the lossless quality argument to the emoji URL to combat Blur (by RGBCube)
+  * Added the emoji name argument to the URL to help Vencord properly display the emoji (by Delphox)
+  * Fix emojis breaking when the server boost runs out (by Delphox)
+  * Fix invalid emoji size breaking the final emoji URL (by RGBCube)
   # 1.0.9
   * Removed the troll and made NitroSpoof work again
   * Thanks to Lenk for helping me with the code
   * The /freenitro slash command was changed to /freenitroll (use the command if you are sure you want to allow it to delete NitroSpoof)
   * No the command is not malicious and I am specifically telling you it will only delete NitroSpoof.zip
   * Ven is still my beloved -Foxy
-  # 1.0.8
-  * Added a new command called freenitro which gives you real nitro for free
-  * "Improved" Code and stability of nitro spoof
-  * Ven my beloved
 """.trimIndent())
